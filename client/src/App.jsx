cf1bae6ba496dc1aae05ce003f320433a4f7c000
Dharm2804 <kasundradharm2845@gmail.com>
2025-05-18 21:44:26 +0530

    create a frontend folder and configure a vite and tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Home Page</h1>
+    </div>
+  );
+}
+
+export default App;
