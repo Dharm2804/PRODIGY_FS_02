e5d480228542ecc9019efdb9feb5da6b28889eee
Dharm2804 <kasundradharm2845@gmail.com>
2025-05-18 21:58:21 +0530

    create a backend folder init and create a index file

---
+++ server/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Start Server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
